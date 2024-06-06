29cdc390c266bc13f70f6f95cad5cb5ee484c29f
yuesui <yuesuirenqianli@outlook.com>
2024-06-06 17:41:45 +0800

    refactor: design mode

--- example/lucky.py
+++ example/lucky.py
@@ -16,6 +16,61 @@
 import webbrowser
 import requests
 import bs4
+from abc import ABC, abstractmethod
+
+
+class FetchStrategy(ABC):
+    @abstractmethod
+    def fetch(self, url, headers):
+        pass
+
+
+class SimpleFetchStrategy(FetchStrategy):
+    def fetch(self, url, headers):
+        try:
+            res = requests.get(url=url, headers=headers)
+            res.raise_for_status()
+            return res.text
+        except requests.RequestException as e:
+            print(f'Error fetching {url}: {e}')
+            return None
+
+
+class ParseStrategy(ABC):
+    @abstractmethod
+    def parse(self, html):
+        pass
+
+
+class SimpleParseStrategy(ParseStrategy):
+    def parse(self, html):
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+        links = soup.select('.item a')
+        return [link.get('href') for link in links]
+
+
+class ScraperFactory:
+    @abstractmethod
+    def create_scraper(fetch_strategy: FetchStrategy, parser_strategy: ParseStrategy):
+        return Scraper(fetch_strategy, parser_strategy)
+
+
+class Scraper:
+    def __init__(self, fetch_strategy: FetchStrategy, parser_strategy: ParseStrategy):
+        self.fetch_strategy = fetch_strategy
+        self.parser_strategy = parser_strategy
+
+    def scrape_adn_open(self, url, headers):
+        html = self.fetch_strategy.fetch(url, headers)
+        if html:
+            links = self.parser_strategy.parse(html)
+            self.open_links(links)
+
+    @staticmethod
+    def open_links(links):
+        num_open = min(5, len(links))
+        for i in range(num_open):
+            webbrowser.open(links[i])
 
 
 def main():
@@ -26,14 +81,10 @@ def main():
         'User-Agent': '',
         'Cookie': ''
     }
-    res = requests.get(url=url, headers=headers)
-
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-
-    links = soup.select('.item a')
-    numOpen = min(5, len(links))
-    for i in range(numOpen):
-        webbrowser.open(links[i].get('href'))
+    fetch_strategy = SimpleFetchStrategy()
+    parser_strategy = SimpleParseStrategy()
+    scraper = ScraperFactory.create_scraper(fetch_strategy, parser_strategy)
+    scraper.scrape_adn_open(url, headers)
 
 
 if __name__ == '__main__':
